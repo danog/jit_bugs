41318197065da49d8565b038abc78fc50c874317
Daniil Gentili <daniil@daniil.it>
2025-05-04 14:52:03 +0000

    Improve

--- bugs/18_all.sh
+++ bugs/18_all.sh
@@ -4,6 +4,6 @@ cd $PWD/bugs
 
 echo "About to run nightly.php"
 
-docker run -v $PWD:/app --rm --privileged -it asan_tests bash -c 'export USE_ZEND_ALLOC=0; /usr/bin/php /app/nightly.php'
+docker run -v $PWD:/app -v $PWD/../tmp:/tmp --rm --privileged -it asan_tests bash -c 'export USE_ZEND_ALLOC=0; /usr/bin/php /app/nightly.php'
 
 echo "OK, no bugs!"
